0542ac26a172f3ea1cde426d3e32b4dcf263ee33
Charley <charleychen2006@gmail.com>
2025-09-20 23:44:34 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+<main class="flex flex-col justify-center items-center w-full h-dvh gap-4 bg-stone-900 p-10">
+  <img class="h-24 w-auto mb-4" src="/images/logo.png" alt="Logo">
+  <h1 class="text-center text-3xl font-black text-transparent bg-clip-text bg-gradient-to-r from-green-700 to-green-500">ZooomAround.com</h1>
+  <p class="text-center text-xl font-bold text-stone-100">Binnenkort beschikbaar</p>
+  <p class="text-center text-stone-700">Zooom Around Late 2025</p>
+</main>
+
